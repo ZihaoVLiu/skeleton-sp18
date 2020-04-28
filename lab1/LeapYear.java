b3baeca9b48ea68967202c19e23f23fdf5137596
Zihao <44451166+ZihaoVLiu@users.noreply.github.com>
2020-04-28 19:36:41 -0400

    test change

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -13,6 +13,14 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
+    
+    private static void isLeapYear(int year) {
+        if (isLeapYear(year)) {
+            System.out.printf("%d is a leap year.\n", year);
+        } else {
+            System.out.printf("%d is not a leap year.\n", year);
+        }
+    }
 
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
