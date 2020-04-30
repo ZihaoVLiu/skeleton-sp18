34b871f5e5971b54f9239f75808d65a266b90bba
Zihao <44451166+ZihaoVLiu@users.noreply.github.com>
2020-04-29 23:48:00 -0400

    Modify dcatenate function

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,10 +81,10 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        if (A.rest == null){
-            A.rest = B;
+        if (A == null){
+            A = B;
         } else {
-            dcatenate(A.rest, B);
+            A.rest = dcatenate(A.rest, B);
         }
         return A;
     }
