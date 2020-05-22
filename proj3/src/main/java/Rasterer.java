888287a4258f564d13824c300a658c910c566ea6
Zihao <44451166+ZihaoVLiu@users.noreply.github.com>
2020-05-22 19:06:43 -0400

    Proj3 part1 done

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -8,6 +8,22 @@ import java.util.Map;
  * not draw the output correctly.
  */
 public class Rasterer {
+    private final double ULLON = -122.2998046875;
+    private final double ULLAT = 37.892195547244356;
+    private final double LRLON = -122.2119140625;
+    private final double LRLAT = 37.82280243352756;
+    private double LONDPP = (LRLON - ULLON) / 256;
+    private double paramUllon;
+    private double paramLrlon;
+    private double paramUllat;
+    private double paramLrlat;
+    private double paramW;
+    private double paramH;
+    private int level;
+    private int size;
+    private double width;
+    private double height;
+    private boolean query_success = false;
 
     public Rasterer() {
         // YOUR CODE HERE
@@ -42,10 +58,138 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
+        System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+//        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
+//                + "your browser.");
+
+        getParamsInfo(params);
+        int[] indexes = getCornerLonLatIndex(params);
+        double[] latlon = getCornerLonLat(indexes);
+        String renders[][] = getRenders(indexes);
+        ifSuccess();
+
+        results.put("render_grid", renders);
+        results.put("raster_ul_lon", latlon[0]);
+        results.put("raster_ul_lat", latlon[1]);
+        results.put("raster_lr_lon", latlon[2]);
+        results.put("raster_lr_lat", latlon[3]);
+        results.put("depth", level);
+        results.put("query_success", query_success);
+        return results;
+    }
+
+    private void getParamsInfo(Map<String, Double> params) {
+        paramUllon = params.get("ullon");
+        paramLrlon = params.get("lrlon");
+        paramUllat = params.get("ullat");
+        paramLrlat = params.get("lrlat");
+        paramW = params.get("w");
+        paramH = params.get("h");
+    }
+
+    private int getLevel(Map<String, Double> params) {
+        double paramLONDPP = (paramLrlon - paramUllon) / paramW;
+        for (int level = 0; level < 8; level++) {
+            if (paramLONDPP >= (LONDPP / (int)Math.pow(2, level))) {
+                return level;
+            }
+        }
+        return 7;
+    }
+
+    private int getSize(int level) {
+        return (int)Math.pow(2, level);
+    }
+
+    private int[] getCornerLonLatIndex(Map<String, Double> params) {
+        int[] lonLatIndex = new int[4];
+        level = getLevel(params);
+        size = getSize(level);
+        width = (LRLON - ULLON) / size;
+        height = (ULLAT - LRLAT) / size;
+
+        if (paramUllon < ULLON) {
+            lonLatIndex[0] = 0;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (ULLON + width * i <= paramUllon && paramUllon < ULLON + width * (i + 1)) {
+                    lonLatIndex[0] = i;
+                    break;
+                }
+            }
+        }
+
+        if (paramUllat > ULLAT) {
+            lonLatIndex[1] = 0;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (ULLAT - height * i >= paramUllat && paramUllat > ULLAT - height * (i + 1)) {
+                    lonLatIndex[1] = i;
+                    break;
+                }
+            }
+        }
+
+        if (paramLrlon > LRLON) {
+            lonLatIndex[2] = size - 1;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (LRLON - width * i >= paramLrlon && paramLrlon > LRLON - width * (i + 1)) {
+                    lonLatIndex[2] = size - 1 - i;
+                    break;
+                }
+            }
+        }
+
+        if (paramLrlat < LRLAT) {
+            lonLatIndex[3] = size - 1;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (LRLAT + height * i <= paramLrlat && paramLrlat < LRLAT + height * (i + 1)) {
+                    lonLatIndex[3] = size - 1- i;
+                    break;
+                }
+            }
+        }
+        query_success = true;
+        return lonLatIndex;
+    }
+
+    private void ifSuccess() {
+        if ((paramUllat > ULLAT && paramLrlat > ULLAT) ||
+                (paramUllat < LRLAT && paramLrlat < LRLAT) ||
+                (paramUllon < ULLON && paramLrlon < ULLON) ||
+                (paramUllon > LRLON && paramLrlon > LRLON)) {
+            query_success = false;
+        }
+        if (paramUllon > paramLrlon || paramUllat < paramLrlat) {
+            query_success = false;
+        }
+    }
+
+    private String[][] getRenders(int[] indexes) {
+        int column = indexes[2] - indexes[0] + 1;
+        int row = indexes[3] - indexes[1] + 1;
+        String[][] renders = new String[row][column];
+        int x;
+        int y;
+        for (int i = 0; i < row; i++) {
+            y = i + indexes[1];
+            for (int j = 0; j< column; j++) {
+                x = j + indexes[0];
+                renders[i][j] = "d" + level +"_x" + x +"_y" + y + ".png";
+            }
+        }
+        return renders;
+    }
+
+    private double[] getCornerLonLat(int[] indexes) {
+        double[] results = new double[4];
+        results[0] = ULLON + width * indexes[0];
+        results[1] = ULLAT - height * indexes[1];
+        results[2] = ULLON + width * (indexes[2] + 1);
+        results[3] = ULLAT - height * (indexes[3] + 1);
         return results;
     }
 
